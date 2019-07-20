f55a7a921fdf92a036226cb41ae4a15b05102789
shin <kouokukou@yahoo.co.jp>
2019-07-20 19:22:43 +0900

    two

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module Urlweb
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.autoload_paths << Rails.root.join("lib")
-
+    config.assets.initialize_on_precompile = false
 
 
     # Settings in config/environments/* take precedence over those specified here.
