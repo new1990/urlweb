a1b3f0bce1a5d2bde97aa5d499f6290049ac5705
shin <kouokukou@yahoo.co.jp>
2019-07-21 16:15:14 +0900

    heroku用に編集

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :configure_sign_up_params, only: [:create]
-  before_action :configure_account_update_params, only: [:update]
-
+  # before_action :configure_sign_up_params, only: [:create]
+  # before_action :configure_account_update_params, only: [:update]
+  #
   # GET /resource/sign_up
   # def new
   #   super
@@ -37,8 +37,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def cancel
   #   super
   # end
-
-  protected
+  #
+  # protected
   #
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
@@ -49,12 +49,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def configure_account_update_params
   #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
   # end
-
+  #
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
   #   super(resource)
   # end
-
+  #
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  # devise_for :users
+  devise_for :users
    get "top" => "contents#top"
    get "top/:id" => "contents#topfolder"
    post "create" => "contents#create"
